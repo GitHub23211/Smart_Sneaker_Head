2381e92bf316c628e4b79720a9766e8833ce21d6
Himanshi Garg <hima.garg.aus@gmail.com>
2022-09-24 23:55:30 +1000

    implemented the Main Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import {
+  BrowserRouter as Router,
+  Routes, Route , Switch 
+} from "react-router-dom"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+      <NavBar />
+      <Routes>
+        <Route path = "/" element={<Home />} />
+        <Route path = "/login" element={<Login />} />
+        <Route path = "/register" element={<Register />} />
+      </Routes>
+      </div>
+     
+   </Router>
+
+  
   );
 }
 
