90b3c91ad38e088b3390b4d961b0780f2b334cb3
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-27 23:54:50 +1000

    templated product.js functions

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -1,6 +1,37 @@
 const models = require('../models')
+const auth = requre('./auth')
 
-
+/**
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
 const createProduct = async (request, response) => {
+    const seller = await auth.validateUser(request)
+    if(seller) {
+        try {
+            
+        } catch {response.status(400).status({"error": "invalid user"})}
+    }
 
 }
+
+/**
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
+const updateProduct = async (request, response) => {
+
+}
+
+/**
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
+const deleteProduct = async (request, response) => {
+
+}
+
+module.exports = {createProduct, updateProduct, deleteProduct}
