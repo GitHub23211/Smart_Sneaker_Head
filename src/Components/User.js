fb44c92227b6b87157d9522172a19efdfb416664
Himanshi Garg <hima.garg.aus@gmail.com>
2022-09-27 17:47:15 +1000

    modified

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,4 +1,40 @@
+import { Drawer ,List , ListItem , ListItemText} from "@mui/material";
+import React from "react";
 
+
+
+const User = () =>{
+
+    return(
+     <>
+        
+        <Drawer 
+       
+          variant="persistent"
+          anchor="right"
+          open={true}
+          
+          >
+            <List>
+                <ListItem>
+                    <ListItemText>Home</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>My Profile</ListItemText>
+                </ListItem><ListItem>
+                    <ListItemText>details</ListItemText>
+                </ListItem>
+            </List>
+          </Drawer>
+     </>
+       
+    )
+}
+
+
+
+
+export default User;
 
 
 
