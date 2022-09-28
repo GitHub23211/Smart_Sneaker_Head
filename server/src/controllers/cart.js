9e07c7361297137d9038500a66928db87dfe7248
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-28 15:29:17 +1000

    implemented addToCart

---
+++ server/src/controllers/cart.js
@@ -0,0 +1,27 @@
+const models = require('../models')
+const auth = require('./auth')
+
+/**
+ * Adds productID to cart field of a user document in the database
+ * @param {Object} request Object containing a params field that is a JSON object with key productid
+ * @param {Object} response Object for returning json responses 
+ * @returns 200 status on success, else 40x codes on errors
+ */
+const addToCart = async (request, response) => {
+    const buyer = await auth.validateUser(request)
+    if(buyer) {
+        const user = await models.Session.findById(buyer)
+        const newCart = user.cart.concat([request.params.productid])
+        user.cart = newCart
+        await user.save()
+            .catch(e => {
+                response.status(400).json({error: "unable to add to cart", error: e})
+            })
+        return response.status(200).json({status: "successfully added item to cart", newCart: user.cart})
+    }
+    return response.status(401).json({error: "invalid user"})
+}
+
+
+
+module.exports = {addToCart}
