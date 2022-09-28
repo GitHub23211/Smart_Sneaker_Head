c28572cf302d33c40380942cd44e80f19a17d604
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-28 18:04:15 +1000

    addToCart now increases quantity if same item added to cart more than once

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -1,10 +1,6 @@
 const models = require('../models')
 const auth = require('./auth')
 
-const itemToAdd = (cart) => {
-    
-}
-
 /**
  * Adds productID to cart field of a user document in the database
  * @param {Object} request Object containing a params field that is a JSON object with key productid
@@ -15,11 +11,21 @@ const addToCart = async (request, response) => {
     const buyer = await auth.validateUser(request)
 
     if(buyer) {
-
         const user = await models.Session.findById(buyer)
         const productid = request.params.productid
-        const newCart = user.cart.concat()
-        user.cart = newCart
+
+        const item = user.cart.findIndex(product => product.productid.toString() === productid)
+        if(item > -1) {
+            user.cart[item].quantity += 1
+        }
+        else {
+            const itemToAdd = {
+                productid: productid,
+                quantity: 1
+            }
+            const newCart = user.cart.concat(itemToAdd)
+            user.cart = newCart
+        }
 
         await user.save()
             .catch(e => {
@@ -31,6 +37,12 @@ const addToCart = async (request, response) => {
     return response.status(401).json({error: "invalid user"})
 }
 
+/**
+ * Deletes item from cart
+ * @param {Object} request Object containing a params field that is a JSON object with key productid
+ * @param {Object} response Object for returning json responses 
+ * @returns 200 status on success, else 40x codes on errors
+ */
 const deleteFromCart = async (request, response) => {
     const buyer = await auth.validateUser(request)
 
@@ -38,7 +50,7 @@ const deleteFromCart = async (request, response) => {
 
         const productToDelete = request.params.productid
         const user = await models.Session.findById(buyer)
-        const newCart = user.cart.filter(item => item.product.toString() != productToDelete)
+        const newCart = user.cart.filter(item => item.productid.toString() != productToDelete)
         user.cart = newCart
 
         await user.save()
