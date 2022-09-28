f06adeaccff3bdb5597a2a01ebc6262c707f5779
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-28 17:20:45 +1000

    basic delete function that deletes all instances of a product from cart

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -26,6 +26,24 @@ const addToCart = async (request, response) => {
     return response.status(401).json({error: "invalid user"})
 }
 
+const deleteFromCart = async (request, response) => {
+    const buyer = await auth.validateUser(request)
 
+    if(buyer) {
 
-module.exports = {addToCart}
+        const productToDelete = request.params.productid
+        const user = await models.Session.findById(buyer)
+        const newCart = user.cart.filter(item => item.product.toString() != productToDelete)
+        user.cart = newCart
+
+        await user.save()
+            .catch(e => {
+                response.status(400).json({error: "unable to delete from cart", error: e})
+            })
+
+        return response.status(200).json({status: "successfully deleted item from cart", newCart: user.cart})
+    }
+    return response.status(401).json({error: "invalid user"})
+}
+
+module.exports = {addToCart, deleteFromCart}
