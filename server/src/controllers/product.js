86297842496bcee7fb70028f599a57f87ce01be3
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-28 13:41:59 +1000

    implemented product deletion from database, updated comments

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -2,9 +2,10 @@ const models = require('../models')
 const auth = require('./auth')
 
 /**
- * Creates new product to put into database
+ * Creates new product and puts it into the database
  * @param {Object} request - Object containing a body field that is a JSON object with 4 keys: name, price, description, quantity
  * @param {Object} response - Object used to send a json response.
+ * @returns 200 status on success, 400 status if product exists, 401 status if unknown error or seller could not be validated
  */
 const createProduct = async (request, response) => {
     const seller = await auth.validateUser(request)
@@ -34,9 +35,10 @@ const createProduct = async (request, response) => {
 }
 
 /**
- * 
- * @param {*} request 
- * @param {*} response 
+ * Update a product in the database given its ID and seller ID
+ * @param {Object} request 
+ * @param {Object} response 
+ * @returns 200 status on success, 401 status if unknown error or is invalid seller
  */
 const updateProduct = async (request, response) => {
     const seller = await auth.validateUser(request)
@@ -50,22 +52,31 @@ const updateProduct = async (request, response) => {
                 quantity: request.body.quantity
             }
             const productToUpdate = await models.Product.findOneAndUpdate(filter, updatedProduct)
-            return response.status(200).json({status: "successfully updated product", pro: productToUpdate, up: updatedProduct})
+            return response.status(200).json({status: "successfully updated product", before: productToUpdate, after: updatedProduct})
         }
         catch {return response.status(401).json({error: "failed to update product"})}
     }
-    return response.status(401).json({status: "invalid seller"})
+    return response.status(401).json({error: "invalid seller"})
 }
 
 /**
- * 
- * @param {*} request 
- * @param {*} response 
+ * Delete a product in the database given its ID and seller ID
+ * @param {Object} request 
+ * @param {Object} response 
+ * @returns 200 status on success, 401 status if unknown error or is invalid seller
  */
 const deleteProduct = async (request, response) => {
     const seller = await auth.validateUser(request)
     if(seller) {
+        try {
+            const filter = {_id: request.params.productid, seller: seller}
+            const productToDelete = await models.Product.find(filter)
+            await models.Product.deleteOne(filter)
+            return response.status(200).json({status: "successfully deleted product", productDeleted: productToDelete})
+        }
+        catch {return response.status(401).json({error: "failed to delete product"})}
     }
+    return response.status(401).json({error: "invalid seller"})
 }
 
 module.exports = {createProduct, updateProduct, deleteProduct}
