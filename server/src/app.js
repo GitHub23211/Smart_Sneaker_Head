c2b9d7923ca197a30afc21b3d886878b9ebae642
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-29 23:39:19 +1000

    implemented middleware into app

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./routes')
+const auth = require('./auth_routes')
+const verifyUser = require('./middleware/auth')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use('/auth', auth)
+app.use(verifyUser.authenticate)
 app.use('/', router)
 
 module.exports = app
