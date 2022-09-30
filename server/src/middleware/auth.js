948585ee92a89dcf770557a9664ef5eac82bcf97
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-01 00:28:36 +1000

    changed to use async/await

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,15 +1,12 @@
 const auth = require('../controllers/auth')
 
-const authenticate = (request, response, next) => {
-    auth.validateUser(request)
-    .then(user => {
-        if(user) {
-            request.user = user
-            return next()
-        }
-        throw new Error("invalid user")
-    })
-    .catch(e => response.status(401).json({error: e}))
+const authenticate = async (request, response, next) => {
+    const user = await auth.validateUser(request)
+    if(user) {
+        request.user = user
+        return next()
+    }
+    return response.status(401).json({error: "invalid user"})
 }
 
 module.exports = {authenticate}
