236861f7a1a45cb913d1bb9a961e1f67b956477c
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-01 01:57:51 +1000

    improved error handling, added try catch blocks

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -10,26 +10,26 @@ const addToCart = async (request, response) => {
     const buyer = request.user
 
     if(buyer) {
-        const user = await models.Session.findById(buyer)
-        const productid = request.body.productid
+        try {
+            const user = await models.Session.findById(buyer)
+            const productid = request.body.productid
 
-        const itemInCart = user.cart.some(product => product.productid.toString() === productid)
-        if(!itemInCart) {
-            const itemToAdd = {
-                productid: productid,
-                quantity: request.body.quantity
+            const itemInCart = user.cart.some(product => product.productid.toString() === productid)
+            if(!itemInCart) {
+                const itemToAdd = {
+                    productid: productid,
+                    quantity: request.body.quantity
+                }
+                const newCart = user.cart.concat(itemToAdd)
+
+                user.cart = newCart
+
+                await user.save()
+
+                return response.status(200).json({status: "successfully added item to cart", newCart: user.cart})
             }
-            const newCart = user.cart.concat(itemToAdd)
-
-            user.cart = newCart
-
-            await user.save()
-            .catch(e => {
-                response.status(400).json({error: "unable to add to cart", error: e})
-            })
-            return response.status(200).json({status: "successfully added item to cart", newCart: user.cart})
-        }
-        return response.status(400).json({error: "item already in cart"})
+            return response.status(400).json({error: "item already in cart"})
+        } catch(e) {return response.status(400).json({error: e})}
     }
     return response.status(401).json({error: "invalid user"})
 }
@@ -44,19 +44,20 @@ const addToCart = async (request, response) => {
     const buyer = request.user
 
     if(buyer) {
-        const user = await models.Session.findById(buyer)
-        const productToUpdate = request.body.productid
-        const itemIndex = user.cart.findIndex(item => item.productid.toString() === productToUpdate)
-
-        if(itemIndex > -1) {
-            user.cart[itemIndex].quantity = request.body.quantity
-            await user.save()
-            .catch(e => {
-                response.status(400).json({error: "unable to update quantity", error: e})
-            })
-            return response.status(200).json({status: "successfully updated item quantity", newCart: user.cart})
+        try {
+            const user = await models.Session.findById(buyer)
+            const productToUpdate = request.body.productid
+            const itemIndex = user.cart.findIndex(item => item.productid.toString() === productToUpdate)
+    
+            if(itemIndex > -1) {
+                user.cart[itemIndex].quantity = request.body.quantity
+    
+                await user.save()
+                return response.status(200).json({status: "successfully updated item quantity", newCart: user.cart})
+            }
+            return response.status(400).json({error: "could not find item to update"})
         }
-        return response.status(400).json({error: "could not find item to update"})
+        catch(e) {return response.status(401).json({error: e})}
     }
     return response.status(401).json({error: "invalid user"})
 }
@@ -71,17 +72,20 @@ const deleteFromCart = async (request, response) => {
     const buyer = request.user
 
     if(buyer) {
-        const productToDelete = request.params.productid
-        const user = await models.Session.findById(buyer)
-        const newCart = user.cart.filter(item => item.productid.toString() != productToDelete)
-        user.cart = newCart
-
-        await user.save()
-            .catch(e => {
-                response.status(400).json({error: "unable to delete from cart", error: e})
-            })
-
-        return response.status(200).json({status: "successfully deleted item from cart", newCart: user.cart})
+        try {
+            const productToDelete = request.params.productid
+            const user = await models.Session.findById(buyer)
+            const newCart = user.cart.filter(item => item.productid.toString() != productToDelete)
+            user.cart = newCart
+    
+            await user.save()
+                .catch(e => {
+                    response.status(400).json({error: "unable to delete from cart", error: e})
+                })
+    
+            return response.status(200).json({status: "successfully deleted item from cart", newCart: user.cart})
+        }
+        catch(e) {return response.status(401).json({error: e})}
     }
     return response.status(401).json({error: "invalid user"})
 }
