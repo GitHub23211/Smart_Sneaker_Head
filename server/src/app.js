07eb67e128f152ff9014d48ce91295010a75d364
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-12 16:36:34 +1100

    restructured express app for seller routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const upload = require('./routes/upload_routes')
-const routes = require('./routes/routes')
+const userRoutes = require('./routes/user_routes')
+const sellerRoutes = require('./routes/seller_routes')
 const authRoutes = require('./routes/auth_routes')
-const verifyUser = require('./middleware/auth')
+const upload = require('./routes/upload_routes')
+const verification = require('./middleware/auth')
 
 const app = express()
 
@@ -13,9 +14,12 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use('/user/image', express.static('./server/src/pictures/avatars'))
 app.use('/product/image', express.static('./server/src/pictures/products'))
-app.use('/', routes)
-app.use(verifyUser.authenticate)
-app.use('/', upload)
 app.use('/', authRoutes)
+app.use(verification.verifyUser)
+app.use('/', userRoutes)
+app.use('/', upload)
+app.use(verification.verifySeller)
+app.use('/', sellerRoutes)
+
 
 module.exports = app
