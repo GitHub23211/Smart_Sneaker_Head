03e04cc787d37911ff935bea61feb3d6710ebe1b
Himanshi Garg <hima.garg.aus@gmail.com>
2022-10-12 23:02:54 +1100

    modified front end design as per new model

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Typography, Button} from "@mui/material";
+import { Button , Paper} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import '../Styles/product.css';
@@ -22,26 +22,34 @@ const Product =()=>{
 
   //axios call to get product details
      return(
+
+     <Paper elevation={24} style={{ width:"auto",height:"auto" ,backgroundColor:"#f2c988",margin:"100px"}}>
+
      <section className="product-container">
       <section className="product-img-section">
            <img className="product-img" src={`/product/image/${product.picture}`} alt=''></img>
       </section>
 
       <section className="product-details-section">
-
           <h2 className="product-title">{product.name}</h2>
          <p className="product-descripti}on">Description : {product.description} </p>
          <p className="product-price"> price : AU${product.price}</p>
         
          <section className="counter" >
-         <Button style={{backgroundColor:"white", color: 'black' }} variant="contained" onClick={IncNum} ><AddIcon /></Button>
-            <Typography>{count}</Typography>
-            <Button style={{backgroundColor:"white", color: 'black' }}  variant="contained" onClick={DecNum}><RemoveIcon /></Button>
+         <Button style={{backgroundColor:"white", color: 'black',margin:'auto 10px' }} variant="contained" onClick={IncNum} ><AddIcon /></Button>
+            <h2>{count}</h2>
+            <Button style={{backgroundColor:"white", color: 'black', margin:'auto 10px'}}  variant="contained" onClick={DecNum}><RemoveIcon /></Button>
          </section>
-         <Button>Add to Cart</Button>
+         <section>
+         <Button variant="contained" style={{backgroundColor:"white",color:"black"}}>Add to Cart</Button>
+         </section>
+        
 
       </section>
      </section>
+
+     </Paper> 
+
 
     )
 }
---
+++ src/Components/UserComponents/Wishlist.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Wishlist = ()=>{
+
+    return(
+        <section>
+          <h1>Wishlist</h1>
+          <p>Your wishlist is empty</p>
+        </section>
+       
+    )
+}
+
+
+export default Wishlist;
