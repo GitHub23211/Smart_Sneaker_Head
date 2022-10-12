df0bd5af7467426fee85239b71a72a474177d53a
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-12 18:51:51 +1100

    added comments

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -6,7 +6,7 @@ const models = require('../models')
  * @param {Object} request Object containing Authorization token
  * @param {Object} response Object for returning json responses 
  * @param {Function} next Callback argument to continue middleware execution
- * @returns Passes control to next middleware that contains routes that require a verified user, else returns error.
+ * @returns Passes control to next pieces of middleware user_routes and upload which require a verified user, else returns error.
  */
 const verifyUser = async (request, response, next) => {
     const user = await auth.validateUser(request)
@@ -19,6 +19,13 @@ const verifyUser = async (request, response, next) => {
     }
 }
 
+/**
+ * Checks to see if the verified user is marked as a seller in the database
+ * @param {Object} request Object containing the verified user's token
+ * @param {Object} response Object for returning json responses 
+ * @param {Function} next Callback argument to continue middleware execution
+ * @returns Passes control to next middleware seller_routes, else returns error.
+ */
 const verifySeller = async(request, response, next) => {
     const user = await models.Seller.findOne({_id: request.user})
     if(user) {
