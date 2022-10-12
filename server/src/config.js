db5ad31a2904341fdaa8ab8d6a0715249db8ae81
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-13 00:37:53 +1100

    removed unnecessary code

--- server/src/config.js
+++ server/src/config.js
@@ -4,6 +4,5 @@ const mongoDBUrl = process.env.MONGODB_URL
 const mongoDBUrlTest = process.env.MONGODB_URL_TEST
 const port = process.env.PORT
 const secret = process.env.SECRET_TOKEN
-const nodeEnv = process.env.NODE_ENV
 
-module.exports = {mongoDBUrl, port, secret, mongoDBUrlTest, nodeEnv}
+module.exports = {mongoDBUrl, port, secret, mongoDBUrlTest}
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ const app = require('./app')
 const models = require('./models')
 const config = require('./config')
 
-config.nodeEnv === 'test' ? models.startTestDb() : models.startDb()
+models.startDb()
 
 app.listen(config.port, () => console.log("Server running on port", config.port))
