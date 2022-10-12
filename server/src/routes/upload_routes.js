7a7c33161219338ba36e3d5f04385f8d3dbdfb5a
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-12 18:56:16 +1100

    added upload route for company logos

--- server/src/routes/upload_routes.js
+++ server/src/routes/upload_routes.js
@@ -3,18 +3,25 @@ const upload = require('../middleware/upload')
 const router = express.Router()
 
 const avatarDest = './server/src/pictures/avatars'
+const logoDest = './server/src/pictures/logos'
 const productDest = './server/src/pictures/products'
 
-/* POST request to upload a user avatar to backend */
+/* POST request to upload user avatar to backend */
 router.post('/api/upload/avatar', upload.createUpload(avatarDest).single('avatar'), (req, res) => {
     const filename = res.req.file.filename
-    return res.status(200).json({status: "avatar upload successful!", filename: filename})
+    return res.status(200).json({status: "avatar upload successful", filename: filename})
 })
 
-/* POST request to upload a product picture to backend */
+/* POST request to upload company logo to backend */
+router.post('/api/upload/logo', upload.createUpload(logoDest).single('logo'), (req, res) => {
+    const filename = res.req.file.filename
+    return res.status(200).json({status: "logo upload successful", filename: filename})
+})
+
+/* POST request to upload product picture to backend */
 router.post('/api/upload/product', upload.createUpload(productDest).single('product'), (req, res) => {
     const filename = res.req.file.filename
-    return res.status(200).json({status: "product image upload successful!", filename: filename})
+    return res.status(200).json({status: "product image upload successful", filename: filename})
 })
 
 module.exports = router
