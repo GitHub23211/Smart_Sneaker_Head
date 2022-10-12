dfd55b82b784b78c3f92c0acf7127b2d5e9c280c
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-12 16:36:17 +1100

    Added seller routes. Only sellers can register and update products now.

--- server/src/routes/auth_routes.js
+++ server/src/routes/auth_routes.js
@@ -1,33 +1,19 @@
 const express = require('express')
 const product = require('../controllers/product')
-const cart = require('../controllers/cart')
-const profile = require('../controllers/profile')
 const auth = require('../controllers/auth')
 
 const router = express.Router()
 
-/* GET request that returns currently logged in user */
-router.get('/auth', auth.getUser)
+/* GET request to get all products */
+router.get('/api/product', product.getProducts)
 
-/* POST request to register product on to website for sale */
-router.post(`/api/product/register`, product.createProduct)
+/* POST request that registers user to website */
+router.post('/auth/register/user', auth.createUser)
 
-/* PUT request to edit product details */
-router.put(`/api/product/update/:productid`, product.updateProduct)
+/* POST request that registers seller to website */
+router.post('/auth/register/seller', auth.createSeller)
 
-/* DELETE request to remove a product for sale */
-router.delete(`/api/product/delete/:productid`, product.deleteProduct)
+/* POST request to log user into website */
+router.post(`/auth/login`, auth.loginUser)
 
-/* PUT request to add products to cart */
-router.put(`/api/cart/add/:productid`, cart.addToCart)
-
-/* PUT request to update quantity of item in cart */
-router.put(`/api/cart/update/:productid`, cart.updateQuantity)
-
-/* DELETE request to remove a product from cart */
-router.delete(`/api/cart/delete/:productid`, cart.deleteFromCart)
-
-/* PUT request to update user profile information */
-router.put('/api/profile/update', profile.updateProfile)
-
-module.exports = router 
+module.exports = router
---
+++ server/src/routes/user_routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cart = require('../controllers/cart')
+const profile = require('../controllers/profile')
+const auth = require('../controllers/auth')
+
+const router = express.Router()
+
+/* GET request that returns currently logged in user */
+router.get('/auth', auth.getUser)
+
+/* PUT request to add products to cart */
+router.put(`/api/cart/add/:productid`, cart.addToCart)
+
+/* PUT request to update quantity of item in cart */
+router.put(`/api/cart/update/:productid`, cart.updateQuantity)
+
+/* DELETE request to remove a product from cart */
+router.delete(`/api/cart/delete/:productid`, cart.deleteFromCart)
+
+/* PUT request to update user profile information */
+router.put('/api/profile/update', profile.updateProfile)
+
+module.exports = router 
