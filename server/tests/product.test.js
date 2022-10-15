932e511d5ab212235706494b9986da0a03bfc279
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-15 13:59:11 +1100

    finished update product tests

--- server/tests/product.test.js
+++ server/tests/product.test.js
@@ -3,9 +3,42 @@ const app = require('../src/app')
 
 const api = supertest(app)
 
+const registerUser = async () => {
+    const data = {
+        username: "testproduct",
+        password: "123",
+        email: "product@testuser"
+    }
+
+    await api.post("/auth/register/user")
+    .send(data)
+    .expect(201)
+}
+
 const registerSeller = async (data) => {
     const response = await api.post("/auth/register/seller").send(data)
     expect(response.status).toBe(201)
+}
+
+const getUserToken = async () => {
+    let token = ""
+
+    const data = {
+        username: "testproduct",
+        password: "123"
+    }
+
+    await api.post("/auth/login")
+    .send(data)
+    .expect(200)
+    .expect(response => {
+        expect(response.body.status).toBe("success")
+        expect(response.body.token).not.toBeNull()
+        expect(response.body.token).not.toBeUndefined()
+        token = response.body.token
+    })
+
+    return token
 }
 
 const getSeller1Token = async () => {
@@ -50,6 +83,7 @@ beforeAll(async () => {
 
     await registerSeller(seller1)
     await registerSeller(seller2)
+    await registerUser()
     const token1 = await getSeller1Token()
     const token2 = await getSeller2Token()
 
@@ -175,23 +209,8 @@ describe("Testing product API endpoints", () => {
         })
 
         test("User tries to create a product", async () => {
-            let token = ""
 
-            const data = {
-                username: "testproduct",
-                password: "123",
-                email: "product@testuser"
-            }
-
-            await api.post("/auth/register/user")
-            .send(data)
-            .expect(201)
-            .expect(response => {
-                expect(response.body.status).toBe("success")
-                expect(response.body.token).not.toBeNull()
-                expect(response.body.token).not.toBeUndefined()
-                token = response.body.token
-            })
+            const token = await getUserToken()
 
             const product = {
                 name: "test",
@@ -264,10 +283,265 @@ describe("Testing product API endpoints", () => {
     })
 
     describe("Test update product", () => {
+
+        test("Update product", async () => {
+
+            const token = await getSeller1Token()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Sneakers"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(2)
+                        expect(response.body.products[1].name).toBe("Sneakers")
+                        productid = response.body.products[1].id
+                     })
+            
+            const updatedInfo = {
+                name: "Sneakers updated",
+                price: 999,
+                description: "updated",
+                quantity: 0
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .set('Authorization', `Bearer ${token}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(200)
+
+            await api.get('/api/product')
+            .query({name: "Sneakers"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[1].name).toBe("Sneakers updated")
+               expect(response.body.products[1].price).toBe(999)
+               expect(response.body.products[1].description).toBe("updated")
+               expect(response.body.products[1].quantity).toBe(0)
+            })
         
+
+        })
+
+        test("Update product partially", async () => {
+
+            const token = await getSeller1Token()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Shoes"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(2)
+                        expect(response.body.products[1].name).toBe("Shoes")
+                        productid = response.body.products[1].id
+                     })
+            
+            const updatedInfo = {
+                price: 999,
+                description: "updated"
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .set('Authorization', `Bearer ${token}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(200)
+
+            await api.get('/api/product')
+            .query({name: "Shoes"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[1].name).toBe("Shoes")
+               expect(response.body.products[1].price).toBe(999)
+               expect(response.body.products[1].description).toBe("updated")
+               expect(response.body.products[1].quantity).toBe(20)
+            })
+        
+
+        })
+        
+        test("Update product with bad token", async () => {
+
+            const token = await getSeller1Token() + "bad token"
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Sneakers updated"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Sneakers updated")
+                        productid = response.body.products[0].id
+                     })
+            
+            const updatedInfo = {
+                name: "Sneakers updated bad token",
+                price: 999,
+                description: "updated",
+                quantity: 0
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .set('Authorization', `Bearer ${token}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(401)
+            expect(response.body.error).toBe("invalid user")
+
+            await api.get('/api/product')
+            .query({name: "Sneakers"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[1].name).toBe("Sneakers updated")
+               expect(response.body.products[1].price).toBe(999)
+               expect(response.body.products[1].description).toBe("updated")
+               expect(response.body.products[1].quantity).toBe(0)
+            })
+        })
+
+        test("Update product with no token", async () => {
+
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Sneakers updated"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Sneakers updated")
+                        productid = response.body.products[0].id
+                     })
+            
+            const updatedInfo = {
+                name: "Sneakers updated bad token",
+                price: 999,
+                description: "updated",
+                quantity: 0
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(401)
+            expect(response.body.error).toBe("invalid user")
+
+            await api.get('/api/product')
+            .query({name: "Sneakers"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[1].name).toBe("Sneakers updated")
+               expect(response.body.products[1].price).toBe(999)
+               expect(response.body.products[1].description).toBe("updated")
+               expect(response.body.products[1].quantity).toBe(0)
+            })
+        })
+        
+        test("Update product of other seller", async () => {
+
+            const token = await getSeller1Token()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Other Sneakers"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Other Sneakers")
+                        productid = response.body.products[0].id
+                     })
+            
+            const updatedInfo = {
+                name: "Other Sneakers updated by seller1",
+                price: 999,
+                description: "updated",
+                quantity: 0
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .set('Authorization', `Bearer ${token}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(401)
+            expect(response.body.error).toBe("Error: Not the original seller or product no longer exists")
+
+            await api.get('/api/product')
+            .query({name: "Sneakers"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[0].name).toBe("Other Sneakers")
+               expect(response.body.products[0].price).toBe(100)
+               expect(response.body.products[0].description).toBe("Some other sneakers")
+               expect(response.body.products[0].quantity).toBe(5)
+            })
+        })
+
+        test("User tries to update product", async () => {
+
+            const token = await getUserToken()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Other Sneakers"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Other Sneakers")
+                        productid = response.body.products[0].id
+                     })
+            
+            const updatedInfo = {
+                name: "Other Sneakers updated by user",
+                price: 999,
+                description: "updated",
+                quantity: 0
+            }
+            const response = await api.put(`/api/product/update/${productid}`)
+                                      .set('Authorization', `Bearer ${token}`)
+                                      .send(updatedInfo)
+            
+            expect(response.status).toBe(401)
+            expect(response.body.error).toBe("invalid seller")
+
+            await api.get('/api/product')
+            .query({name: "Sneakers"})
+            .expect(response => {
+               expect(response.body.products).toHaveLength(2)
+               expect(response.body.products[0].name).toBe("Other Sneakers")
+               expect(response.body.products[0].price).toBe(100)
+               expect(response.body.products[0].description).toBe("Some other sneakers")
+               expect(response.body.products[0].quantity).toBe(5)
+            })
+        })
     })
 
     describe("Test delete product", () => {
+
+        test("delete a product", async () => {
+            const token = await getSeller1Token()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Other Sneakers"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Other Sneakers")
+                        productid = response.body.products[0].id
+                     })
+            
+            const response = await api.delete(`/api/product/delete/${productid}`).set('Authorization', `Bearer ${token}`)
+
+            expect(response.status).toBe(200)
+            expect(response.body.status).toBe("success")
+            expect(response.body.product.name).toBe("Other Sneakers")
+            expect(response.body.product.id).toBe(productid)
+        })
+
+        test("try to delete a product from another seller", async () => {
+            const token = await getSeller2Token()
+            let productid = ""
+            await api.get('/api/product')
+                     .query({name: "Other Sneakers"})
+                     .expect(response => {
+                        expect(response.body.products).toHaveLength(1)
+                        expect(response.body.products[0].name).toBe("Other Sneakers")
+                        productid = response.body.products[0].id
+                     })
+            
+            const response = await api.delete(`/api/product/delete/${productid}`).set('Authorization', `Bearer ${token}`)
+
+            expect(response.status).toBe(401)
+            expect(response.body.status).toBe("invalid seller")
+            expect(response.body.product.name).toBe("Other Sneakers")
+            expect(response.body.product.id).toBe(productid)
+        })
+
+        test("try to update deleted product", async () => {
+            
+        })
         
     })
 
