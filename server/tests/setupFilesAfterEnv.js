7197a468b00b78e4113e3b1bdcf7f1437f40d60c
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-16 23:26:32 +1100

    added cart api tests. Modified test environment config.

--- server/tests/setupFilesAfterEnv.js
+++ server/tests/setupFilesAfterEnv.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const models = require('../src/models')
 const config = require('../src/config')
 
 beforeAll(async () => {
@@ -7,6 +8,12 @@ beforeAll(async () => {
         await mongoose.connect(config.mongoDBUrlTest)
         console.log('connected to database on port:', config.port)
         console.log("on mongoURL", config.mongoDBUrlTest)
+        await models.Session.deleteMany({})
+        console.log("Deleted all users")
+        await models.Seller.deleteMany({})
+        console.log("Deleted all sellers")
+        await models.Product.deleteMany({})
+        console.log("Deleted all products")
     } catch(e) {console.log("error occured", e.toString())}
 })
 
