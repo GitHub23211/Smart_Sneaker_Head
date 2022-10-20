b0b8831ea0c054130ba3aac89825c6390e732a00
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-20 12:03:22 +1100

    set up stripe controller and stripe API key

--- server/src/config.js
+++ server/src/config.js
@@ -5,5 +5,6 @@ const mongoDBUrlTest = process.env.MONGODB_URL_TEST
 const port = process.env.PORT
 const secret = process.env.SECRET_TOKEN
 const guid = process.env.GUID
+const stripe = process.env.STRIPE_TOKEN
 
-module.exports = {mongoDBUrl, port, secret, mongoDBUrlTest, guid}
+module.exports = {mongoDBUrl, port, secret, mongoDBUrlTest, guid, stripe}
