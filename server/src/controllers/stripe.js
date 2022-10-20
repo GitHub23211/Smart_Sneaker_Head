8e787947d2468e37ee955300b2efddda06a1541e
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-10-20 12:05:45 +1100

    set up basic stripe endpoint

--- server/src/controllers/stripe.js
+++ server/src/controllers/stripe.js
@@ -1,2 +1,15 @@
 const models = require('../models')
 const config = require('../config')
+const stripe = require("stripe")(config.stripe)
+
+const paymentIntent = async (request, response) => {
+    await stripe.paymentIntents.create({
+        amount: 100,
+        currency: "aud",
+        automatic_payment_methods: {
+            enabled: true
+        }
+    })
+
+    response.send({clientSecret: paymentIntent.client_secret})
+}
