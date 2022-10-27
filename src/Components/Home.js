a7f19bd1692c3c1e22fa9777ccc5ef6717772c29
Himanshi Garg <hima.garg.aus@gmail.com>
2022-10-28 00:14:49 +1100

    changed Ui

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Divider } from "@mui/material";
+import { Typography, Divider,Button } from "@mui/material";
 import "../Styles/home.css";
 import axios from "axios";
 import HomePageProduct from "./HomePageProduct";
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Home = () => {
 
@@ -37,33 +42,32 @@ const Home = () => {
     },[]);
    
     return(  
-    <section className="home-container">
-        <section className="home-banner">
-           <h1>Welcome to Smart Sneaker Head!</h1>
-           <Divider>
-           <p> Bring power to your steps</p>
-            </Divider>
-          
-        </section>
-        
-        <section className="home-highlight">
-            <section className="home-highlight-child">
-            Sport keeps us fit. Keeps you mindful. Brings us together. 
-            Through sport we have the power to change lives. Whether it is through stories of inspiring athletes. 
-            Helping you to get up and get moving. Sporting goods featuring the latest technologies, to up your performance. 
-            Beat your PB. Our sport shop offers a home to the runner, the basketball player, the soccer kid, the fitness enthusiast. 
-            The weekend hiker that loves to escape the city. The yoga teacher that spreads the moves. The 3-Stripes are seen in the music scene. On stage, at festivals. 
-            The clothing at our sports store keeps you focused before that whistle blows. During the race. And at the finish lines. 
-            We’re here to support creators. Improve their game. Their lives. And change the world.
-            </section>
-            <section className="home-highlight-child">
-                Put some colour here or image of a logo or shoe
-            </section>
 
-        </section>
+     <>
 
-        <section className="home-products">
-                <Carousel slide={false}>
+    <section className="top-bar">
+      <Carousel indicators={false} >
+        <Carousel.Item>
+          <h2>Welcome to Smart Sneaker Head!</h2>
+        </Carousel.Item>
+        <Carousel.Item>
+             <p>Free Delivery for all orders over $150.</p>
+        </Carousel.Item>
+       </Carousel>
+      </section>
+
+       <section className="home-banner-1">
+          <img src="./images/Sneaker_Image_Left.png" width="500px" height="500px" alt="left"/>
+          <img src="./images/Quote_Image.png" width="600px" height="500px" alt="quote"/>
+          <img src="./images/Sneaker_Image_Right.png" width="500px" height="500px" alt="right"/>
+       </section>
+
+       <section className="home-banner-2">
+       <img src="./images/banner_2_Image.png" width="auto" height="auto" alt="left"/>
+       </section>
+
+       <section className="home-products">
+                <Carousel slide={false} variant="dark">
                   {tmpProdList.map( (p) => {
                     return (
                       <Carousel.Item>
@@ -74,23 +78,49 @@ const Home = () => {
                     )
                     }
                  )}
-                 </Carousel>
+                </Carousel>
+        </section>
+        <section className="home-banner-3">
+         <img src="./images/banner3_image_1.png" width="100px" height="auto" alt="left"/>
+         <img src="./images/banner3_image_2.png" width="100px" height="auto" alt="left"/>
+         <img src="./images/banner3_image_3.png" width="120px" height="auto" alt="left"/>
+         <img src="./images/banner3_image_4.png" width="100px" height="auto" alt="left"/>
+         <img src="./images/banner3_image_5.png" width="100px" height="auto" alt="left"/>
+       </section>
+
+       <section className="home-about-section">
+
+        <section className="about-section-right">
+         <h1>Authenticity Guaranteed</h1>
         </section>
 
-        <section className="home-about-us">
-          <h2>ABOUT</h2>
-          <p>Our vision </p>
-          <p>We see a world where everybody is an athlete — united in the joy of movement. Driven by our passion for sport and our instinct for innovation, we aim to bring inspiration to every athlete in the world and to make sport a daily habit.</p>
+        <section className="about-section-left">
+         <h1 className="about-section-title">Smart Sneaker Head</h1>
+         <h2 className="about-section-title">About</h2>
+            <Button size="large" variant="contained" sx={{backgroundColor:"white",color:"black",borderRadius:25 , mt:"280px",ml:"250px"}}>Search More</Button>
         </section>
-        <section className="home-footer">
+
+       </section>
+
+       <section className="home-footer">
             <Divider>
             <Typography>
                  © Smart Sneaker Head, 2022. All Rights Reserved.
             </Typography>
             </Divider>
+            <InstagramIcon />
+            <FacebookIcon />
+            <TwitterIcon />
+            <YouTubeIcon />
+
         </section>
-    </section>
+
+       
+
+       </>
+
     )
+
 }
 
 export default Home;
