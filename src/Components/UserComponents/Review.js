4a9adc403cf646588cc39700f7cdc7c60a89d5e3
Himanshi Garg <hima.garg.aus@gmail.com>
2022-11-03 16:20:56 +1100

    style changes

--- src/Components/UserComponents/Review.js
+++ src/Components/UserComponents/Review.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react"
-import {Grid, TextField, Rating} from "@mui/material";
+import {Grid, TextField, Rating,Typography, Divider} from "@mui/material";
 import axios from "axios"
 
 const Review = ({review}) => {
@@ -13,15 +13,14 @@ const Review = ({review}) => {
 
     return (
         <>
-        <h3>{user}</h3>
-        <Grid style={{maxWidth: "50%", marginLeft: "25%", marginBottom: "30px"}}>
-            <Rating size="large" name="read-only" value={review.rating} readOnly />
-
-            <TextField label='Title' fullWidth style={{margin: "30px auto"}}
-            InputProps={{readOnly: true}} value={review.title}></TextField>
-
-            <TextField label='Review' fullWidth multiline rows="10"
-            InputProps={{readOnly: true}} value={review.contents}></TextField> 
+       
+        <Grid style={{maxWidth: "50%", marginLeft: "25%", marginBottom: "30px" }}>
+            <h4>Reviews</h4>
+           <section >
+            <h4> <Rating size="medium" name="read-only" value={review.rating} readOnly/> By : {user}</h4>
+           </section>
+            <Typography>{review.title} : {review.contents} </Typography>
+            <Divider />
         </Grid>
         </>
     )
