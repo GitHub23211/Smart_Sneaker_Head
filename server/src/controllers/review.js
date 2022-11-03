a0dbf87069b1f9deae9a8f66bb6962a4544ea2bf
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-11-03 14:58:00 +1100

    set up review backend api

---
+++ server/src/controllers/review.js
@@ -0,0 +1,19 @@
+const models = require('../models')
+
+const getReviews = async (request, response) => {
+    
+}
+
+const createReview = async (request, response) => {
+
+}
+
+const deleteReview = async (request, response) => {
+    
+}
+
+module.exports = {
+    getReviews,
+    createReview,
+    deleteReview
+}
