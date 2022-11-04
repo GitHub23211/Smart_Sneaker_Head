6b3aa169fd890c94793b74d0c5fad0aea5c98fba
Himanshi Garg <hima.garg.aus@gmail.com>
2022-11-04 16:48:34 +1100

    modified

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {Link} from 'react-router-dom';
-import { useContext } from 'react';
 
 import { AppBar, Toolbar, TextField, InputAdornment, IconButton, Box, Menu, MenuItem} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
@@ -9,33 +8,38 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 import LoginContext from '../LoginContext';
 import ProductListContext from "../ProductListContext";
-import { BorderBottomRounded } from "@mui/icons-material";
 
 const NavBar = () =>{
+
    const {isLogin,loginType} = useContext(LoginContext);
    const {setQuery} = useContext(ProductListContext);
 
    const bg ={backgroundColor:'white',margin:'auto'};
    const navStyle = {   background: 'white'}
    const buttonStyle = {backgroundColor:"white", color: 'black' }
+   const linkstyle = {color:"black", textDecoration: 'none'}
+   const menulinkstyle = {color:"black", textDecoration: 'none',mt:"10px"}
 
    const [anchorEl, setAnchorEl] = useState(null);
    const open = Boolean(anchorEl);
+   
    const handleClick = (event) => {
      setAnchorEl(event.currentTarget);
    };
+
    const handleClose = () => {
      setAnchorEl(null);
    };
 
    const handleOnChange = (event, handler) => {
       handler({"name": event.target.value})
-  }
- const handleCategory =(event,brand) =>{
-     setQuery({"brand": brand})
-  }
+   };
 
-    if( isLogin === false) {
+   const handleCategory =(event,brand) =>{
+     setQuery({"brand": brand})
+   };
+
+   if( isLogin === false) {
       return(
          <AppBar position="relative" style={navStyle}>
             <Toolbar>
@@ -68,10 +72,10 @@ const NavBar = () =>{
                   'aria-labelledby': 'basic-button',
                }}
                >
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Nike")}>Nike</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Adidas")}>Adidas</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Jordans")}>Jordans</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Asics")}>Asics</MenuItem></Link>
+                <Link to = "/productlist"  style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Nike")}>Nike</MenuItem></Link>
+                <Link to = "/productlist"  style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Adidas")}>Adidas</MenuItem></Link>
+                <Link to = "/productlist"  style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Jordans")}>Jordans</MenuItem></Link>
+                <Link to = "/productlist"  style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Asics")}>Asics</MenuItem></Link>
 
                </Menu>
                <Box sx={{marginLeft:"550px", width: '30%', pb:"10px",pt:"10px"}}>
@@ -90,14 +94,11 @@ const NavBar = () =>{
                                      }}
                  />
                </Box>
-               
-
-
 
                <Box sx={{marginLeft:"auto"}}>
                    <Link to ="/login" style={{color:"black" , marginLeft:"20px", textDecoration: 'none',marginRight:"20px"}} >LOGIN</Link>
                </Box>
-               <Box sx={{marginLeft:"10px"}}> <Link to ="/register" style={{color:"black" , textDecoration: 'none'}} >REGISTER</Link></Box>
+               <Box sx={{marginLeft:"10px"}}> <Link to ="/register" style={linkstyle} >REGISTER</Link></Box>
             </Toolbar>
          </AppBar>   
       )
@@ -111,7 +112,7 @@ const NavBar = () =>{
                   sx={{ pb: "10px"}}>
                <Link to ="/"><img src="./logo.png" width="370px" height="80px" alt="logo"/></Link>
                </Box>
-               <Box sx={{marginLeft:"20PX"}}> <Link onClick = {(event) => handleCategory(event, "")} style={{color:"black" , textDecoration: 'none'}} to = "/productlist">SNEAKERS</Link></Box>
+               <Box sx={{marginLeft:"20PX"}}> <Link onClick = {(event) => handleCategory(event, "")} style={linkstyle} to = "/productlist">SNEAKERS</Link></Box>
 
                <Box sx={{marginLeft:"20PX"}}> 
                <Link  style={{color:"black" , textDecoration: 'none',mt:"10px"}}
@@ -134,10 +135,10 @@ const NavBar = () =>{
                   'aria-labelledby': 'basic-button',
                }}
                >
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Nike")}>Nike</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Adidas")}>Adidas</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Jordans")}>Jordans</MenuItem></Link>
-                <Link to = "/productlist"  style={{color:"black" , textDecoration: 'none',mt:"10px"}}><MenuItem onClick = {(event) => handleCategory(event, "Asics")}>Asics</MenuItem></Link>
+                <Link to = "/productlist" style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Nike")}>Nike</MenuItem></Link>
+                <Link to = "/productlist" style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Adidas")}>Adidas</MenuItem></Link>
+                <Link to = "/productlist" style={menulinkstyle}><MenuItem onClick = {(event) => handleCategory(event, "Jordans")}>Jordans</MenuItem></Link>
+                <Link to = "/productlist" ><MenuItem onClick = {(event) => handleCategory(event, "Asics")}>Asics</MenuItem></Link>
 
                </Menu>
                <Box sx={{marginLeft:"550px", width: '30%', pb:"10px",pt:"10px"}}>
