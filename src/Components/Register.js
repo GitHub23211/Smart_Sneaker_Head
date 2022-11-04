d740f93668d543af5025eff095bf192d6fb1ed5f
Himanshi Garg <hima.garg.aus@gmail.com>
2022-11-04 16:37:28 +1100

    modified

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -7,34 +7,32 @@ import "../Styles/register.css";
 
 const Register = () =>{
 
-
   const paperStyle = {padding:20, height:'auto', width:'100vh', margin:'60px auto'};
+  const register_button = {backgroundColor:'white', margin:"20px 10px", color: "black"}
+  const linkstyle = {color:"black", textDecoration: 'none'}
+
     return(
 
-       <Paper elevation={2} style={paperStyle}>
- 
-      <section className="register-container">
-        
-        <section className="register">
-         <h1>REGISTRATION</h1>   
-         <p className="register-text">Become a Smart Sneaker Seller!
-        <Link  style={{color:"black" , textDecoration: 'none'}}  to = "/register/seller">
-            <Button variant="contained" style={{backgroundColor:'white' , margin:"20px 10px",color: "black"}} size="small" endIcon={<SendIcon />}>Seller</Button>
-          </Link>
-        </p>    
-       <Divider>OR</Divider>
-        <p className="register-text"> Start Sneaker Shopping! 
-          <Link style={{color:"black" , textDecoration: 'none'}}  to = "/register/user" >
-            <Button variant="contained"  style={{ backgroundColor:'white', margin:"20px 10px" , color: "black"}} size="small" endIcon={<LocalMallOutlinedIcon />}>User</Button>
-          </Link>
-        </p>  
-        </section>
-       </section>
-
+      <Paper elevation={2} style={paperStyle}>
+         <section className="register-container">
+             <section className="register">
+                <h1>REGISTRATION</h1>   
+                <p className="register-text">Become a Smart Sneaker Seller!
+                <Link  style={linkstyle}  to = "/register/seller">
+                   <Button variant="contained" style={register_button} size="small" endIcon={<SendIcon />}>Seller</Button>
+                </Link>
+                </p>    
+          <Divider>OR</Divider>
+                 <p className="register-text"> Start Sneaker Shopping! 
+                <Link style={linkstyle}  to = "/register/user" >
+                  <Button variant="contained"  style={register_button} size="small" endIcon={<LocalMallOutlinedIcon />}>User</Button>
+                </Link>
+                 </p>  
+               </section>
+          </section>
        </Paper> 
 
     )
 }
-
 
 export default Register;
