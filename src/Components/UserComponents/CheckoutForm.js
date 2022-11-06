1abbd45b82532e54ef854eb9243a904156c02df4
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-11-06 21:53:44 +1100

    changed payment successful redirect to home page

--- src/Components/UserComponents/CheckoutForm.js
+++ src/Components/UserComponents/CheckoutForm.js
@@ -53,7 +53,7 @@ const CheckoutForm = ({}) => {
         const { error } = await stripe.confirmPayment({
             elements,
             confirmParams: {
-                return_url: "http://localhost:3000/user/orderconfirm/"
+                return_url: "http://localhost:3000/"
             }
         })
 
