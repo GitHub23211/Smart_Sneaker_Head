105c55fae546333c498d23003cca3948c9cb75e3
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-11-06 21:31:34 +1100

    stripe API now sends actual item total

--- server/src/controllers/stripe.js
+++ server/src/controllers/stripe.js
@@ -1,15 +1,15 @@
 const config = require('../config')
 const stripe = require("stripe")(config.stripe)
 
+
 const paymentIntent = async (request, response) => {
     const intent = await stripe.paymentIntents.create({
-        amount: 100,
+        amount: request.body.total,
         currency: "aud",
         automatic_payment_methods: {
             enabled: true
         }
     })
-    console.log(intent.client_secret)
 
     return response.status(200).json({clientSecret: intent.client_secret})
 }
